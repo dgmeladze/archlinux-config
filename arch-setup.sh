aaeed4147444f268146e7247ef4a1fa1c5cacb3d
dgmeladze <128248810+dgmeladze@users.noreply.github.com>
2025-07-04 18:16:56 +0300

    Update arch-setup.sh
    changed way to install steam to choose libraries

--- arch-setup.sh
+++ arch-setup.sh
@@ -17,8 +17,8 @@ sudo pacman -Syu --noconfirm
 
 echo "==> Установка пакетов из официального репозитория..."
 sudo pacman -S --noconfirm \
-  git wget curl filezilla steam zsh
-
+  git wget curl filezilla zsh
+sudo pacman -S steam
 echo "==> Установка yay..."
 sudo pacman -S --needed --noconfirm base-devel
 if ! command -v yay >/dev/null 2>&1; then
